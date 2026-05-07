5815f9a09026180f1dad3e425ca3938f52735a09
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-07 19:12:37 +0700

    in giu lieu

---
+++ code001.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("my name is Trung\n");
+    printf("I will become a semiconductor engineer\n");
+    printf("I will work in Japan\n");
+    return 0;
+}    
