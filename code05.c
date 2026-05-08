538af710280826d295a5fbc575fd27b9ab375bf1
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-08 18:52:17 +0700

    if else

---
+++ code05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int n;
+    scanf( "%d",&n);
+    if((n % 2) ==0 ){
+        printf("la so chan");
+    }
+    else{
+        printf("la so le");
+    }
+    return 0;
+}
