8447ce6f74f4bebd7f3b730df1dcb140b21630fe
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-10 17:12:56 +0700

    lenh break

---
+++ code14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i ;
+    int n ;
+    for( ; ; ){
+        printf (" hay nhap so");
+        scanf( "%d", &n);
+        if( n == 0){
+            break;
+        }
+    }
+    return 0;
+}
