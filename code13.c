f07f1b389dc0f5d3e6a48d60f57af483f8dc3c2a
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-10 16:44:22 +0700

    tong cac so hang tu 1 đen n

---
+++ code13.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+int main(){
+    int i ; 
+    int n ;
+    int sum = 0 ;
+    scanf("%d" , &n);
+    for( i = 0 ; i <= n ; i++){
+        sum += i ;
+    }
+    printf("%d" , i );
+    return 0;
+}
