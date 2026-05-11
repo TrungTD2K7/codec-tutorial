9df7ca9fa8287c9b5612f00997b6a736964b8421
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-11 19:42:52 +0700

    chu vi va dien tich

---
+++ code16.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    int a , b;
+    scanf("%d", &a );
+    scanf("%d", &b);
+    printf("chu vi : %d\n" , 2*(a+b));
+    printf("dien tich : %d" , a*b);
+    return 0;
+}
