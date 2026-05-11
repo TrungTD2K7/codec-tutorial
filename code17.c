1c920392003fd6005590c7f4215bbcba89a61023
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-11 20:00:11 +0700

    phep tinh co ban

---
+++ code17.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a , b ;
+    printf("nhap so a");
+    scanf("%d", &a);
+    printf("nhap so b");
+    scanf("%d" , &b);
+    printf("tong : %d\n", a+b );
+    printf("hieu : %d\n", a-b );
+    printf("thuong : %d\n", a/b);
+    printf ("tich : %d", a*b);
+    return 0;
+}
