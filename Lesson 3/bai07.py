18eb569109d34a4bce03e05e777899dab67cab63
trungtran2909 <trungtran.tdtss@gmail.com>
2026-07-06 16:12:29 +0700

    return trong ham

---
+++ Lesson 3/bai07.py
@@ -0,0 +1,7 @@
+def toan():
+    a = input(" nhap so nguyen thu nhat : ")
+    b = input(" nhap so nguyen thu hai : ")
+    sum = int(a) + int(b) 
+    print(f"tong cua 2 so hang la {sum}")
+    return
+toan()
