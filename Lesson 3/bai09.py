34d208d4e8e335eab25f491e2f427e0294c39cdc
trungtran2909 <trungtran.tdtss@gmail.com>
2026-07-06 16:35:22 +0700

    toan tu nang cao

---
+++ Lesson 3/bai09.py
@@ -0,0 +1,14 @@
+a = float(input(" nhap so thu nhat : "))
+toantu = input(" nhap toan tu : ")
+b = float(input(" nhap so thu hai : "))
+
+if ( toantu == "+" ):
+    print(a + b)
+elif ( toantu == "-" ):
+    print(a - b)
+elif ( toantu == "*" ):
+    print(a * b)
+elif ( toantu == "/" ):
+    print( a / b )
+else :
+    print(" occho!! ")
