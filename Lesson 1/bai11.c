8675c4108b0118b74f74db153cd545de9b994870
trungtran2909 <trungtran.tdtss@gmail.com>
2026-07-15 21:24:45 +0700

    pe sap den

---
+++ Lesson 1/bai11.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+  
+int n ;
+scanf("%d",&n);
+int arr[n];
+for(int i = 0 ; i < n ; i++){
+  scanf("%d",&arr[i]);
+}
+  for (int i = 0; i < n-1; i++)
+		for (int j = i+1; j < n; j++)
+			if (arr[i] < arr[j]) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+  
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  for(int i = 0 ; i < n ; i++){
+  	if(arr[i] % 3 == 2){
+  		printf("%d ", arr[i]);
+	  }
+  }
+
+  
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
