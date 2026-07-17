e5eafbddc09f3ea8a34faeba56fb394fb0426358
trungtran2909 <trungtran.tdtss@gmail.com>
2026-07-17 11:41:19 +0700

    on pe ngay cuoi

---
+++ Lesson 1/bai13.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+int n;
+scanf("%d",&n);
+int arr[n][n];
+int sum = 0;
+for (int i = 0 ; i < n ; i++){
+  for(int j = 0; j < n ; j++){
+    scanf("%d", &arr[i][j]);
+      	  if( i == j){
+      sum = sum + arr[i][j];
+}
+  }
+}
+
+  
+  
+  
+  
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+
+printf("%d", sum);
+  
+  
+  
+  
+  
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
