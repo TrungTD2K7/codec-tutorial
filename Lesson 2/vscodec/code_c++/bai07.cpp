a976e54ffa2e2a1150c721bc5add70f4ba77c2e7
trungtran2909 <trungtran.tdtss@gmail.com>
2026-08-11 15:20:06 +0700

    c++ bang cuu chuong

---
+++ Lesson 2/vscodec/code_c++/bai07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i, n , j;
+    cin >> n;
+    for( i = 1 ; i <= 10 ; i ++){
+    	cout << n << " x " << i << " = " << i * n<<endl;
+    }
+	return 0;
+}
